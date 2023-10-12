fcd9fde3a40611a8d7fd3154c11c7a286b16cd9a
SkedaddlesAway <abraxoszz@gmail.com>
2023-10-12 11:48:12 -0500

    implementing javascript form validation

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 //this is one of the scripts of all time
 //responsive nav?
+function validateForm() {
+    let x = document.forms["signupForm"]["firstName"].value;
+    let y = document.forms["signupForm"]["lastName"].value;
+    let z = document.forms["signupForm"]["email"].value;
+    if (x == "" || y==""|| z=="") {
+      alert("All fields must be filled out!");
+      return false;
+    }
+  } 
